3aa9a000fd02f0ba02ba49c58b09de4ea07240f8
patternizer <michael.a.taylor@uea.ac.uk>
2021-08-05 22:30:48 +0100

    Add regex library re and use of letter ID in logic to detect station header

--- read_cruts_dtb.py
+++ read_cruts_dtb.py
@@ -17,6 +17,7 @@
 #------------------------------------------------------------------------------
 import numpy as np
 import pandas as pd
+import re
 
 # Silence library version notifications
 import warnings
